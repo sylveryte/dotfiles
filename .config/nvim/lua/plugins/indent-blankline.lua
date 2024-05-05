4c050df1ead8881e7a7f1cf21245a104102dc0a8
Sandeep Sahani <sylveryte@protonmail.com>
2024-05-05 12:42:39 +0530

    lspsaga added

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,2 @@
+-- No need to copy this inside `setup()`. Will be used automatically.
+require("ibl").setup(config)
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 local map = vim.keymap.set
 -- require('telescope').load_extension('fzf')
 require('telescope').load_extension('zoxide')
+require("telescope").load_extension("gitmoji")
+require('telescope').load_extension('frecency')
+-- require('telescope').load_extension("ag")
 require('telescope').setup {
   matchup = {
     enable = true, -- mandatory, false will disable the whole extension
@@ -25,17 +28,18 @@ require('telescope').setup {
   },
 }
 local builtin = require('telescope.builtin')
--- vim.keymap.set("n", "<leader>m", ":Telescope frecency<CR>")
-vim.keymap.set('n', '<leader>z', ":Telescope zoxide list<CR>")
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = "telescope find files" })
-vim.keymap.set('n', '<leader>c', builtin.git_files, { desc = "telescope git files" })
-vim.keymap.set('n', '<leader>s', builtin.live_grep, { desc = "telescope live grep" })
-vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = "telescope buffers" })
-vim.keymap.set('n', '<leader>j', builtin.grep_string, { desc = "telescope grep string" })
-vim.keymap.set('n', '<leader>k', builtin.oldfiles, { desc = "telescope oldfiles" })
-vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = "telescope current_buffer_fuzzy_find" })
-vim.keymap.set('n', '<leader>t', builtin.treesitter, { desc = "telescope treesitter" })
-vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = "telescope lsp_definitions" })
-vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, { desc = "telescope lsp_implementations" })
-vim.keymap.set('n', 'gr', builtin.lsp_references, { desc="telescope lsp_references" })
-vim.keymap.set('n', '<leader>l', builtin.builtin, { desc="telescope list builtins" })
+map('n','<leader>tg',require('telescope').extensions.gitmoji.gitmoji)
+map("n", "<leader>m", ":Telescope frecency<CR>")
+map('n', '<leader>z', ":Telescope zoxide list<CR>", {desc="Telescope zoxide"})
+map('n', '<leader>f', builtin.find_files, { desc = "telescope find files" })
+map('n', '<leader>c', builtin.git_files, { desc = "telescope git files" })
+map('n', '<leader>s', builtin.live_grep, { desc = "telescope live grep" })
+map('n', '<leader>b', builtin.buffers, { desc = "telescope buffers" })
+map('n', '<leader>j', builtin.grep_string, { desc = "telescope grep string" })
+map('n', '<leader>k', builtin.oldfiles, { desc = "telescope oldfiles" })
+map('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = "telescope current_buffer_fuzzy_find" })
+map('n', '<leader>t', builtin.treesitter, { desc = "telescope treesitter" })
+map('n', 'gd', builtin.lsp_definitions, { desc = "telescope lsp_definitions" })
+map('n', '<leader>gi', builtin.lsp_implementations, { desc = "telescope lsp_implementations" })
+map('n', 'gr', builtin.lsp_references, { desc="telescope lsp_references" })
+map('n', '<leader>l', builtin.builtin, { desc="telescope list builtins" })
