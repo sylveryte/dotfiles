1db733319002bd2af8263c653ab107d596ed1ef9
Sandeep Sahani <sylveryte@protonmail.com>
2024-05-16 22:53:20 +0530

    neorg calendar goto

--- .config/nvim/after/ftplugin/norg.lua
+++ .config/nvim/after/ftplugin/norg.lua
@@ -4,6 +4,7 @@ vim.opt_local.wrap = false -- Disable line wrap
 map("n", "<localleader>c", ":Neorg toggle-concealer<CR>")
 map("n", "<localleader>im", ":Neorg inject-metadata<CR>")
 map("n", "<localleader>b", ":Neorg toc<CR>")
+map("n", "<localleader>g", ":Neorg journal custom<CR>")
 map("n", "<localleader>p", "gg=G<C-o>zR:%s/ SYLNEWLINE/\\r/g<CR>gg=G<C-o>zR", { desc = "Indent using =" })
 
 map("n", "<localleader>k", ":norm gg0}j$gd<CR>", { desc = "Go syl next" })
