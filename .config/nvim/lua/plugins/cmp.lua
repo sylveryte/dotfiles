4d6d760c41533c01d7bed70a1d260964ccdb7095
Sandeep Sahani <sylveryte@protonmail.com>
2024-06-06 08:46:29 +0530

    cmp completino using C-space

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -45,6 +45,7 @@ cmp.setup({
     -- ), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-space>'] = cmp.mapping.complete(),
     ['<C-q>'] = cmp.mapping.abort(),
 
 
