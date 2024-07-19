2c02fdc42ab7fa8c7250826de3af360aecb2e262
Sandeep Sahani <sylveryte@protonmail.com>
2024-07-19 11:24:09 +0530

    Deleting old

--- .config/nvim/__old/plugin_a/ui.lua
+++
@@ -1,3 +0,0 @@
-require('gitsigns').setup()
-require('mini.indentscope').setup()
-require("ibl").setup()
--- .config/nvim/__old/plugin_p/ui.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-lualine/lualine.nvim',
-  'MunifTanjim/nui.nvim',
-  "nvim-tree/nvim-web-devicons",
-  "lukas-reineke/indent-blankline.nvim",
-  'lewis6991/gitsigns.nvim',
-  { 'echasnovski/mini.indentscope', version = false }
-}
