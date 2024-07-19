6dbf7f551832cc12e5fc86e3b14f0b42eb7b0e59
Sandeep Sahani <sylveryte@protonmail.com>
2024-07-19 11:15:24 +0530

    Dadbod added

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -108,6 +108,13 @@ cmp.setup.filetype('gitcommit', {
   })
 })
 
+cmp.setup.filetype({ "sql" }, {
+  sources = {
+    { name = "vim-dadbod-completion" },
+    { name = "buffer" }
+  }
+})
+
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
   mapping = cmp.mapping.preset.cmdline(),
