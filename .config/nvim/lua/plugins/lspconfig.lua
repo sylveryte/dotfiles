8518af99a955febf47637af0bb3a03e300287837
Sandeep Sahani <sylveryte@protonmail.com>
2024-07-24 17:08:34 +0530

    Revert "sql"
    This reverts commit dc84cb4751eaec153abe9e37a5ca589135980c01.

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,15 +3,14 @@ require("mason").setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason-lspconfig").setup({
   ensure_installed = { "lua_ls", "gopls", "angularls", "tailwindcss", "tsserver", "html", "emmet_ls", "prismals",
-    "astro", "marksman", "rust_analyzer", "cssls", "eslint", "sqlls","sqlfluff","sql-formatter" }
+    "astro", "marksman", "rust_analyzer", "cssls", "eslint", "sqls" }
 })
 
 
 lspconfig.astro.setup {}
 lspconfig.emmet_ls.setup {}
 lspconfig.rust_analyzer.setup {}
-lspconfig.sqlls.setup {}
-lspconfig.sqlfluff.setup { }
+lspconfig.sqls.setup {}
 lspconfig.marksman.setup {}
 lspconfig.cssls.setup {}
 lspconfig.prismals.setup {}
