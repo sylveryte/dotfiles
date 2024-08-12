7a231ade82251604dd38980aec6da8948aa63466
Sandeep Sahani <sylveryte@protonmail.com>
2024-08-12 08:54:45 +0530

    Krohnkite and sql configs

--- .config/nvim/after/ftplugin/sql.lua
+++ .config/nvim/after/ftplugin/sql.lua
@@ -1,3 +1,4 @@
 local map = vim.keymap.set
 vim.cmd([[nmap <expr> <localleader>r db#op_exec()]])
 vim.cmd([[xmap <expr> <localleader>r db#op_exec()]])
+vim.cmd([[xmap  <localleader>t :norm ,rap<CR>]])
---
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,4 @@
+vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_execute_on_save = 0
+vim.g.db_ui_save_location = '~/trisha/dbplay'
+vim.cmd([[nmap <localleader>i :DBUIToggle<CR>]])
