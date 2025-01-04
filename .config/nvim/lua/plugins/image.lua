56fbf04e1652f63433af0427e8f491332318ec3b
Sandeep Sahani <sylveryte@protonmail.com>
2025-01-04 15:12:30 +0530

    Images always on complete setup

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -11,8 +11,8 @@ return {
         enabled = true,
         clear_in_insert_mode = true,
         download_remote_images = false,
-        only_render_image_at_cursor = true,
-        floating_windows = true,             -- if true, images will be rendered in floating markdown windows
+        only_render_image_at_cursor = false,
+        floating_windows = false,             -- if true, images will be rendered in floating markdown windows
         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
       },
       neorg = {
@@ -34,7 +34,7 @@ return {
     max_height = nil,
     max_width_window_percentage = nil,
     max_height_window_percentage = 50,
-    window_overlap_clear_enabled = false,                                             -- toggles images when windows are overlapped
+    window_overlap_clear_enabled = true,                                             -- toggles images when windows are overlapped
     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
     editor_only_render_when_focused = false,                                          -- auto show/hide images when the editor gains/looses focus
     tmux_show_only_in_active_window = false,                                          -- auto show/hide images in the correct Tmux window (needs visual-activity off)
