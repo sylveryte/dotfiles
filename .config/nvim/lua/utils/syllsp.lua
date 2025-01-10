7d1eb4d5aa457993981d3a03b8e3ac98ad6ad2e2
Sandeep Sahani <sylveryte@protonmail.com>
2025-01-10 07:21:20 +0530

    Even better wikilink navigation

--- .config/nvim/lua/utils/syllsp.lua
+++ .config/nvim/lua/utils/syllsp.lua
@@ -1,3 +1,5 @@
+-- TODO syltodo use treesitter mabybe
+-- :InspectTree
 local function better_link_action(action)
   if vim.bo.filetype == 'markdown' then
     local line = vim.fn.getline('.')
@@ -5,31 +7,27 @@ local function better_link_action(action)
     local cursor_pos = vim.api.nvim_win_get_cursor(0) -- Get the cursor position (row, col)
     local cursor_col = cursor_pos[2] + 1              -- Convert 0-based to 1-based column index
 
-    local pos = line:find("]")
-    local wikilink = line:find("]]")
+    local wikilink = line:find("%]%]")
 
     -- so we can get around - [ ] [[link]]
     if wikilink then
       if cursor_col < wikilink then
+        local wikilinkstart = line:find('%[%[')
+        print('wikilinkstart',wikilinkstart, 'and ' ,wikilink, 'co',cursor_col)
+        if wikilinkstart <= cursor_col then
+          action()
+          return
+        end
         vim.api.nvim_feedkeys("f];", "n", true)
       else
         vim.api.nvim_feedkeys("F];", "n", true)
       end
-    else
-      if pos then
-        if cursor_col < pos then
-          vim.api.nvim_feedkeys("f]", "n", true)
-        else
-          vim.api.nvim_feedkeys("F]", "n", true)
-        end
-      end
+      vim.schedule(function()
+        action()
+      end)
     end
   end
-  -- since vim.api.nvim_feedkeys is async call
-  -- this vim.schedule is like settimeout with 0
-  vim.schedule(function()
-    action()
-  end)
+  action()
 end
 
 return { better_link_action = better_link_action }
