8ac2fdc4c52067402d6dfc857926ba7caab51067
Sandeep Sahani <sylveryte@protonmail.com>
2025-04-16 20:20:19 +0530

    Update

--- .config/nvim/lua/plugins/ts_comments.lua
+++ .config/nvim/lua/plugins/ts_comments.lua
@@ -4,7 +4,11 @@ return {
   event = "VeryLazy",
   enabled = vim.fn.has("nvim-0.10.0") == 1,
   config = function()
-    require('ts-comments').setup()
+    require('ts-comments').setup({
+      lang={
+        ledger ="; %s"
+      }
+    })
     -- vim.keymap.set({ "n", "v" }, "cm", "gc") -- not working
     vim.cmd([[nmap cm gc]])
     vim.cmd([[vmap cm gc]])
