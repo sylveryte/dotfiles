9bbed66316974f7e706da57f6df1de7447a2edc7
Sandeep Sahani <sylveryte@protonmail.com>
2025-06-11 15:45:38 +0530

    screenshotting

--- .config/niri/recommendations.md
+++ .config/niri/recommendations.md
@@ -9,6 +9,12 @@
 - `swayidle`
 - `nm-applet`
 - `gnome-polkit`
+- Screenshot
+  - instead of flameshot we'll use `grim -g "$(slurp)" - | swappy -f -`
+    - instead of swappy we'll use satty `grim -g "$(slurp)" - | satty -f -`
+  * `flameshot` screenshot use `XDG_CURRENT_DESKTOP=Sway flameshot gui` command
+  * also `satty` can help with annotations of existing file
+    use `--copy-command wl-copy`
 
 ## Fedora / common
 
--- tools/screenshot.sh
+++ tools/screenshot.sh
@@ -7,16 +7,12 @@
 # Distributed under terms of the MIT license.
 #
 
-case "$(printf "select\nfocused\nwhole" | dmenu -p "screenshot")" in
-	"select")
-		spectacle -r
+case "$1" in
+	"swappy")
+		grim -g "$(slurp)" - | swappy -f -
 		# scrot --select ~/trisha/sc/del.png
 		;;
-	"focused")
-		spectacle -a
-		# scrot --focused ~/trisha/sc/del.png
-		;;
-	"whole")
-		scrot ~/trisha/sc/del.png
+	"satty")
+	  grim -g "$(slurp)" - | satty -f - --fullscreen --init-tool arrow --annotation-size-factor 0.7 --no-window-decoration --action-on-enter save-to-clipboard  --early-exit --copy-command wl-copy
 		;;
 esac
